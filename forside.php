c18b1c31917954518f41316938bb999a0533125f
Cecilie <hejove_166@hotmail.com>
2016-12-04 14:29:52 +0100

    Add files via upload

---
+++ forside.php
@@ -0,0 +1,28 @@
+<!doctype html>
+<html>
+<head>
+<meta charset="UTF-8">
+<link rel="stylesheet" type="text/css" href="menu_style.css">
+<title>Forside</title>
+</head>
+
+
+
+<body>
+ <?php /*include kommandoen, inkludere den valgt fil på side, så man "kun" skal rette i den ene fil, og ikke alle 5*/
+		
+	include 'menu.php';?>
+
+       
+        
+    <?php 
+		
+	include 'footer.php';?>
+        
+
+       
+
+</body>
+</html>
+
+
