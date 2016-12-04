574331d68e48d138e03058276cbaf85e1dc4e287
CamillaHB <camilla.boysen@hotmail.com>
2016-12-04 14:57:55 +0100

    Add files via upload

--- footer.php
+++ footer.php
@@ -1,8 +1,5 @@
-
-
-
 <link rel="stylesheet" type="text/css" href="menu_style.css">
 <?php /*seperate footer fil, med den samme inklude kommando på hver af de enkle sider, så man kun skal ændre herinde*/
-echo "<div id="footer">tlf: 23232323. Odsherredsvej 123, 4000 Odsherred . facebook.com/odsherred . mobilepay: 22334455</div>";
+echo "<div id='footer'>" . "<p>tlf: 23232323 . Odsherredsvej 123, 4000 Odsherred . facebook.com/odsherred . mobilepay: 22334455<p>" . "</div>";
 ?>
 
--- menu.php
+++ menu.php
@@ -12,10 +12,10 @@
 
 <ul id="navigation">
 <i><a href="forside.php"<?php if ($curpage == 'forside.php') {echo 'class="active"';}?>>Forside</a></i>
-<i><a href="besøg.php"<?php if ($curpage == 'besøg.php') {echo 'class="active"';}?>>Besøg</a></i>
-<i><a href="om os.php"<?php if ($curpage == 'om os.php') {echo 'class="active"';}?>>Om os</a></i>
+<i><a href="besoeg.php"<?php if ($curpage == 'besoeg.php') {echo 'class="active"';}?>>Besøg</a></i>
+<i><a href="om-os.php"<?php if ($curpage == 'om-os.php') {echo 'class="active"';}?>>Om os</a></i>
 <i><a href="dyrene.php"<?php if ($curpage == 'dyrene.php') {echo 'class="active"';}?>>Dyrerne</a></i>
-<i><a href="støt.php"<?php if ($curpage == 'støt.php') {echo 'class="active"';}?>>Støt</a></i>
+<i><a href="stoet.php"<?php if ($curpage == 'stoet.php') {echo 'class="active"';}?>>Støt</a></i>
 </ul>
 
 
