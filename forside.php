273b073713d20a172f4862f2f081631bf20e4d42
Alauridsen <andrslauridsen@gmail.com>
2016-12-09 11:23:43 +0100

    Add files via upload

--- forside.php
+++ forside.php
@@ -14,11 +14,16 @@ body {
 </head>
 
 <body>
+<div id="fb-root"></div>
+<script src="//connect.facebook.net/en_US/sdk.js#xfbml=1&amp;version=v2.5" 
+  		async></script>
  <?php /*include kommandoen, inkludere den valgt fil på side, så man "kun" skal rette i den ene fil, og ikke alle 5*/
 include 'menu.php';?>
 
 <h1>Velkommen!</h1>
-	
+
+<div class="fb-post" data-href="https://www.facebook.com/OdsherredZooRescue/photos/a.213727227807.131311.161716077807/10154700396077808/?type=3&amp;theater" data-width="500" data-show-text="true"><blockquote cite="https://www.facebook.com/OdsherredZooRescue/posts/10154700396077808:0" class="fb-xfbml-parse-ignore"><p>M&#xe5;ske man skulle v&#xe6;re et hulepindsvin og bare ligge og tage solbad. zoohilsner Joan</p>Opslået af <a href="https://www.facebook.com/OdsherredZooRescue/">Odsherred Zoo</a> på&nbsp;<a href="https://www.facebook.com/OdsherredZooRescue/posts/10154700396077808:0">7. december 2016</a></blockquote></div>    
+    
 <?php include 'footer.php';?>
         
 
